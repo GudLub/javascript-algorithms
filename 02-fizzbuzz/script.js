95a42ff5128079b3619472eace8b79ff2f917be8
GudLub <107323910+GudLub@users.noreply.github.com>
2022-09-08 16:10:32 +0300

    Update script.js

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -10,9 +10,18 @@
  * 
 */
 
-function fizzBuzz(num) {
-    // Напишите код здесь
-}
+function fizzBuzz (numb) {
+    for (let i = 1; i <= numb; i++)
+    if ( i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+    } else if ( i % 3 === 0) { 
+        console.log('fizz');
+    } else if ( i % 5 === 0) {
+        console.log('buzz');
+    } else {
+       console.log(i);
+    }
+};
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
