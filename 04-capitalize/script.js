4faf2d59a03d835d41dc6faf2e84b3872cbaed7b
GudLub <107323910+GudLub@users.noreply.github.com>
2022-09-08 16:14:45 +0300

    Update script.js

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -6,8 +6,11 @@
  *
 */
 
-function capitalize(str) {
-    // Напишите код здесь
+function capitalize (str) {
+    let arr = str.split(' ');
+    for (i = 0; i < arr.length; i++)
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1); 
+    return arr.join(' ');
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
